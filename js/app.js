ec2f92adb2f899a8db6262dd40ea7bea79148931
iqbal kang <iqbal.kang@yahoo.com>
2021-01-26 13:30:29 -0800

    Fianl project

--- js/app.js
+++ js/app.js
@@ -1,28 +1,24 @@
-// ********** set date ************
-// select span
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
 
 // ********** nav toggle ************
 // select button and links
-const navBtn = document.getElementById("nav-toggle");
-const links = document.getElementById("nav-links");
+const navBtn = document.getElementById('nav-toggle');
+const links = document.getElementById('nav-links');
 // add event listener
-navBtn.addEventListener("click", () => {
-  links.classList.toggle("show-links");
+navBtn.addEventListener('click', () => {
+  links.classList.toggle('show-links');
+  console.log(navBtn);
 });
 
 // ********** smooth scroll ************
 // select links
-const scrollLinks = document.querySelectorAll(".scroll-link");
-scrollLinks.forEach(link => {
-  link.addEventListener("click", e => {
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
     // prevent default
     e.preventDefault();
-    links.classList.remove("show-links");
+    links.classList.remove('show-links');
 
-    const id = e.target.getAttribute("href").slice(1);
+    const id = e.target.getAttribute('href').slice(1);
     const element = document.getElementById(id);
     //
     let position = element.offsetTop - 62;
@@ -31,7 +27,7 @@ scrollLinks.forEach(link => {
       left: 0,
       // top: element.offsetTop,
       top: position,
-      behavior: "smooth"
+      behavior: 'smooth',
     });
   });
 });
